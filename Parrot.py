7e2a7757ceeec9e9ac584ca3fbf37c2f4de7aa85
Jeremy Galloway <JeremyNGalloway@gmail.com>
2013-06-04 15:10:38 -0500

    What's a quine you ask?

--- Parrot.py
+++ Parrot.py
@@ -1,6 +1,7 @@
 #Jeremy Galloway JeremyNGalloway@gmail.com
 #This simple python module echos the source code of the program it's being called from.
 #Implement via a "--source" argument, for example
+#http://en.wikipedia.org/wiki/Quine_%28computing%29
 
 import sys
 
